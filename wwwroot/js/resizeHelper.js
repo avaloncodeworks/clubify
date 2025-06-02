ac35c3315eccbd5ea97e94638333dcb179005760
Avalon Codeworks <avaloncodeworks@gmail.com>
2025-06-02 19:35:13 +0100

    DW020625-2

---
+++ wwwroot/js/resizeHelper.js
@@ -0,0 +1,14 @@
+﻿window.resizeHelper = {
+    registerResizeHandler: function (dotNetHelper) {
+        function checkSize() {
+            const width = window.innerWidth;
+            dotNetHelper.invokeMethodAsync('OnResize', width);
+        }
+
+        // Fire once at registration
+        checkSize();
+
+        // Listen for resize events
+        window.addEventListener('resize', checkSize);
+    }
+};
